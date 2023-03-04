1cc9f21fbd01cdf88cc64d3ec0e068f962f3b797
BoatFloater <theboatfloater@gmail.com>
2023-03-04 12:27:47 +0900

    initial commit

---
+++ Assets/Scenes/mastersync/Teleporter.cs
@@ -0,0 +1,38 @@
+﻿
+using MMMaellon;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Teleporter : UdonSharpBehaviour
+{
+    [SerializeField] private SmartObjectSync[] smartSync;
+    [SerializeField] private Transform[] target;
+    
+    void Start()
+    {
+    }
+
+    public void Teleport1()
+    {
+        this.internalTeleport(0);
+    }
+
+    public void Teleport2()
+    {
+        this.internalTeleport(1);
+    }
+
+    public void Teleport3()
+    {
+        this.internalTeleport(2);
+    }
+
+    private void internalTeleport(int id)
+    {
+        Debug.Log("internal teleport");
+        Networking.SetOwner(Networking.LocalPlayer, this.smartSync[id].gameObject);
+        this.smartSync[id].TeleportTo(this.target[id].position, this.target[id].rotation, Vector3.zero, Vector3.zero);
+    }
+}
